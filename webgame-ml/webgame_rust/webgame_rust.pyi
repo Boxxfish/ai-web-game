03ec7d26a648ddcea081cbcf18935bf18b5690b7
Ben Giacalone <bensgiacalone@gmail.com>
2024-02-17 01:25:28 -0500

    Add game wrapper from Rust end

--- webgame-ml/webgame_rust/webgame_rust.pyi
+++ webgame-ml/webgame_rust/webgame_rust.pyi
@@ -1,8 +1,53 @@
-from typing import Tuple
+from typing import *
 
-class CartpoleEnv:
+class PyVec2:
+    """
+    Represents a 2D vector.
+    """
+    x: float
+    y: float
+
+class AgentState:
+    """
+    Contains the state of an agent for a single frame.
+    """
+    pos: PyVec2
+    dir: PyVec2
+
+class GameState:
+    """
+    Contains the state of the game for a single frame.
+    """
+    player: AgentState
+    pursuer: AgentState
+    walls: list[bool]
+    level_size: int
+
+class AgentAction:
+    """
+    Indicates the kind of actions an agent can take.
+    """
+    NoAction: int
+    MoveUp: int
+    MoveUpRight: int
+    MoveRight: int
+    MoveDownRight: int
+    MoveDown: int
+    MoveDownLeft: int
+    MoveLeft: int
+    MoveUpLeft: int
+
+class GameWrapper:
     def __init__(self) -> None: ...
     def step(
-        self, action: int
-    ) -> Tuple[Tuple[float, float, float, float], float, bool]: ...
-    def reset(self) -> Tuple[float, float, float, float]: ...
+        self, action_player: AgentAction, action_pursuer: AgentAction
+    ) -> GameState:
+        """
+        Runs one step of the game, and returns the next state of the game.
+        """
+        ...
+    def reset(self) -> GameState: 
+        """
+        Resets the game, returning the next state of the game.
+        """
+        ...
