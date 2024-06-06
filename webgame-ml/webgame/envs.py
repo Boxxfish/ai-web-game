069910661b7a41952db623e278e86231d174b4f4
Ben Giacalone <bensgiacalone@gmail.com>
2024-06-06 17:43:19 -0400

    Set up trajectory recording

--- webgame-ml/webgame/envs.py
+++ webgame-ml/webgame/envs.py
@@ -15,14 +15,11 @@ OBJ_DIM = 8
 # The world space size of a grid cell
 CELL_SIZE = 25
 
-class GameEnv(pettingzoo.ParallelEnv):
+class BaseGameEnv(pettingzoo.ParallelEnv):
     """
     An environment that wraps an instance of our game.
 
     Agents: pursuer, player
-
-    Observation Space: A tuple, where the first item is a list of vectors representing visible objects in the game from
-    an agent's POV, and the second item is a 2D map showing where walls are.
 
     Action Space: Discrete, check the `AgentAction` enum for a complete list.
 
@@ -49,7 +46,7 @@ class GameEnv(pettingzoo.ParallelEnv):
             all_actions.append(actions[agent])
         self.game_state = self.game.step(all_actions[0], all_actions[1])
         assert self.game_state
-        obs = game_state_to_obs(self.game_state)
+        obs = self.game_state_to_obs(self.game_state)
         self.last_obs = obs
         rewards = {
             "player": 0.0,
@@ -74,16 +71,42 @@ class GameEnv(pettingzoo.ParallelEnv):
     ) -> tuple[Mapping[str, tuple[np.ndarray, np.ndarray]], dict[str, None]]:
         self.game_state = self.game.reset()
         assert self.game_state
-        obs = game_state_to_obs(self.game_state)
+        obs = self.game_state_to_obs(self.game_state)
         infos = {
             "player": None,
             "pursuer": None,
         }
         return (obs, infos)
+    
+    def game_state_to_obs(
+        self, game_state: GameState,
+    ) -> Mapping[str, tuple[np.ndarray, np.ndarray]]:
+        """
+        Converts the game state to our expected observations.
+        """
+        return {
+            "player": self.agent_state_to_obs(game_state.player, game_state, False),
+            "pursuer": self.agent_state_to_obs(game_state.pursuer, game_state, True),
+        }
+    
+    def agent_state_to_obs(
+        self, agent_state: AgentState, game_state: GameState, is_pursuer: bool
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Generates observations for an agent.
+        """
+        raise NotImplementedError()
 
     @functools.lru_cache(maxsize=None)
     def action_space(self, _agent: str) -> gym.Space:
         return gym.spaces.Discrete(10)
+
+
+class ObjsGameEnv(BaseGameEnv):
+    """
+    Observation Space: A tuple, where the first item is a list of vectors representing visible objects in the game from
+    an agent's POV, and the second item is a 2D map showing where walls are.
+    """
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, _: str) -> gym.Space:
@@ -93,55 +116,85 @@ class GameEnv(pettingzoo.ParallelEnv):
                 gym.spaces.Box(0, 1, (self.level_size, self.level_size)),
             )
         )
+    
+    def agent_state_to_obs(
+        self, agent_state: AgentState, game_state: GameState, is_pursuer: bool
+    ) -> tuple[np.ndarray, np.ndarray]:
+        obs_vecs = np.zeros([MAX_OBJS, OBJ_DIM], dtype=float)
+        for i, e in enumerate(agent_state.observing):
+            obs_obj = game_state.objects[e]
+            obj_features = np.zeros([OBJ_DIM])
+            obj_features[0] = 0.5 + obs_obj.pos.x / (game_state.level_size * CELL_SIZE)
+            obj_features[1] = 0.5 + obs_obj.pos.y / (game_state.level_size * CELL_SIZE)
+            obj_features[2] = 1
+            if e in agent_state.vm_data:
+                vm_data = agent_state.vm_data[e]
+                obj_features[5] = 1
+                obj_features[6] = vm_data.last_seen / 10.0
+                obj_features[7] = vm_data.state_changed
+            obs_vecs[i] = obj_features
+        for i, e in enumerate(agent_state.listening):
+            obj_noise = game_state.noise_sources[e]
+            obj_features = np.zeros([OBJ_DIM])
+            obj_features[0] = obj_noise.pos.x / (game_state.level_size * CELL_SIZE)
+            obj_features[1] = obj_noise.pos.y / (game_state.level_size * CELL_SIZE)
+            obj_features[3] = 1
+            obj_features[4] = obj_noise.active_radius
+            obs_vecs[i + len(agent_state.observing)] = obj_features
+        walls = np.array(game_state.walls, dtype=float).reshape(
+            (game_state.level_size, game_state.level_size)
+        )
+        return (obs_vecs, walls)
 
+class VisionGameEnv(BaseGameEnv):
+    """
+    Observation Space: A tuple, where the first item is a vector of the following form:
 
-def game_state_to_obs(
-    game_state: GameState,
-) -> Mapping[str, tuple[np.ndarray, np.ndarray]]:
+    0: This agent's x coordinate, normalized between 0 and 1
+    1: This agent's y coordinate, normalized between 0 and 1
+    2: This agent's direction vector's x coordinate, normalized
+    3: This agent's direction vector's y coordinate, normalized
+    4: 1 if the other agent is visible, 0 if not
+    5: If the other agent is visible, the other agent's x coordinate divided by map size
+    6: If the other agent is visible, the other agent's y coordinate divided by map size
+    
+    , and the second item is a 2D map showing where walls are.
     """
-    Converts the game state to our expected observations.
-    """
-    return {
-        "player": agent_state_to_obs(game_state.player, game_state),
-        "pursuer": agent_state_to_obs(game_state.pursuer, game_state),
-    }
 
-
-def agent_state_to_obs(
-    agent_state: AgentState, game_state: GameState
-) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Generates observations for an agent.
-    """
-    obs_vecs = np.zeros([MAX_OBJS, OBJ_DIM], dtype=float)
-    for i, e in enumerate(agent_state.observing):
-        obs_obj = game_state.objects[e]
-        obj_features = np.zeros([OBJ_DIM])
-        obj_features[0] = obs_obj.pos.x / (game_state.level_size * CELL_SIZE)
-        obj_features[1] = obs_obj.pos.y / (game_state.level_size * CELL_SIZE)
-        obj_features[2] = 1
-        if e in agent_state.vm_data:
-            vm_data = agent_state.vm_data[e]
-            obj_features[5] = 1
-            obj_features[6] = vm_data.last_seen / 10.0
-            obj_features[7] = vm_data.state_changed
-        obs_vecs[i] = obj_features
-    for i, e in enumerate(agent_state.listening):
-        obj_noise = game_state.noise_sources[e]
-        obj_features = np.zeros([OBJ_DIM])
-        obj_features[0] = obj_noise.pos.x / (game_state.level_size * CELL_SIZE)
-        obj_features[1] = obj_noise.pos.y / (game_state.level_size * CELL_SIZE)
-        obj_features[3] = 1
-        obj_features[4] = obj_noise.active_radius
-        obs_vecs[i + len(agent_state.observing)] = obj_features
-    walls = np.array(game_state.walls, dtype=float).reshape(
-        (game_state.level_size, game_state.level_size)
-    )
-    return (obs_vecs, walls)
+    @functools.lru_cache(maxsize=None)
+    def observation_space(self, _: str) -> gym.Space:
+        return gym.spaces.Tuple(
+            (
+                gym.spaces.Box(0, 1, (7,)),
+                gym.spaces.Box(0, 1, (self.level_size, self.level_size)),
+            )
+        )
+    
+    def agent_state_to_obs(
+        self, agent_state: AgentState, game_state: GameState, is_pursuer: bool
+    ) -> tuple[np.ndarray, np.ndarray]:
+        obs_vec = np.zeros([7], dtype=float)
+        obs_vec[0] = 0.5 + agent_state.pos.x / (game_state.level_size * CELL_SIZE)
+        obs_vec[1] = 0.5 + agent_state.pos.y / (game_state.level_size * CELL_SIZE)
+        obs_vec[2] = agent_state.dir.x
+        obs_vec[3] = agent_state.dir.y
+        
+        other_agent = ["player", "pursuer"][int(is_pursuer)] 
+        other_e, other_obs = list(
+            filter(lambda t: t[1].obj_type == other_agent, game_state.objects.items())
+        )[0]
+        if other_e in agent_state.observing:
+            obs_vec[4] = 1
+            obs_vec[5] = 0.5 + other_obs.pos.x / (game_state.level_size * CELL_SIZE)
+            obs_vec[6] = 0.5 + other_obs.pos.y / (game_state.level_size * CELL_SIZE)
+        walls = np.array(game_state.walls, dtype=float).reshape(
+            (game_state.level_size, game_state.level_size)
+        )
+        return (obs_vec, walls)
 
 
 if __name__ == "__main__":
-    env = GameEnv(visualize=False)
+    env = ObjsGameEnv(visualize=False)
     env.reset()
 
     for _ in tqdm(range(1000)):
---
+++ webgame-ml/webgame/gen_trajectories.py
@@ -0,0 +1,81 @@
+"""
+Script for generating trajectories for training our filter.
+"""
+
+from argparse import ArgumentParser
+import json
+import os
+from pathlib import Path
+import random
+import string
+from typing import *
+import pickle
+
+import numpy as np
+from tqdm import tqdm
+from dataclasses import dataclass
+
+from webgame.envs import CELL_SIZE, VisionGameEnv
+from webgame.filter import pos_to_grid
+
+
+@dataclass
+class TrajDataAll:
+    seqs: List[List[np.ndarray]]
+    tiles: List[List[int]]
+
+
+def process_obs(obs: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+    scalar_obs, grid_obs = obs
+    scalar_size = scalar_obs.shape[0]
+    grid_shape = grid_obs.shape
+    scalar_obs = np.tile(
+        scalar_obs[..., np.newaxis, np.newaxis], [1] + list(grid_shape)
+    )
+    grid_obs = grid_obs[np.newaxis, ...]
+    return np.concatenate([scalar_obs, grid_obs], 0)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--out-dir", type=str, default="./runs")
+    parser.add_argument("--seq-len", type=int, default=32)
+    parser.add_argument("--num-seqs", type=int, default=4_000)
+    args = parser.parse_args()
+
+    env = VisionGameEnv()
+    action_space = env.action_space("pursuer")
+    all_seqs = []
+    all_tiles = []
+    for seq_idx in tqdm(range(args.num_seqs)):
+        seq = []
+        tiles = []
+        for seq_step in range(args.seq_len):
+            obs, infos = env.reset()
+            actions = {}
+            for agent in env.agents:
+                actions[agent] = action_space.sample()
+            obs, rewards_, dones_, truncs_, infos = env.step(actions)
+            pursuer_obs = obs["pursuer"]
+            processed_obs = process_obs(pursuer_obs)
+            player_pos = env.game_state.player.pos
+            gold_tile = pos_to_grid(
+                player_pos.x, player_pos.y, env.game_state.level_size, CELL_SIZE
+            )
+            seq.append(process_obs)
+            tiles.append(gold_tile)
+        all_seqs.append(seq)
+        all_tiles.append(tiles)
+
+    out_dir = Path(args.out_dir)
+    out_id = "".join(
+        [random.choices(string.ascii_letters + string.digits) for _ in range(8)]
+    )
+    traj_data_all = TrajDataAll(seqs=all_seqs, tiles=all_tiles)
+    os.mkdir(out_dir / out_id)
+    with open(out_dir / out_id / "traj_data_all.json", "w") as f:
+        json.dump(traj_data_all.__dict__, f)
+
+
+if __name__ == "__main__":
+    main()
