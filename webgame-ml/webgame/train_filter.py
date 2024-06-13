6e6dc5bc61c05eb7d96e21f148e1a9b1d3f1d37f
Ben Giacalone <bensgiacalone@gmail.com>
2024-06-13 13:40:41 -0400

    Set up obj processing architecture

--- webgame-ml/webgame/train_filter.py
+++ webgame-ml/webgame/train_filter.py
@@ -22,16 +22,66 @@ import wandb
 
 
 class MeasureModel(nn.Module):
-    def __init__(self, channels: int, size: int, use_pos: bool = False):
+    def __init__(
+        self,
+        channels: int,
+        size: int,
+        use_pos: bool = False,
+        objs_shape: Optional[Tuple[int, int]] = None,
+    ):
         super().__init__()
         num_channels = channels
         if use_pos:
             num_channels += 2
-        self.net = nn.Sequential(
+        self.use_objs = objs_shape is not None
+
+        # Grid + scalar feature processing
+        grid_features_dim = 32
+        self.grid_net = nn.Sequential(
             nn.Conv2d(num_channels, 32, 3, padding="same", dtype=torch.double),
             nn.BatchNorm2d(32, dtype=torch.double),
             nn.SiLU(),
             nn.Conv2d(32, 32, 3, padding="same", dtype=torch.double),
+            nn.BatchNorm2d(32, dtype=torch.double),
+            nn.SiLU(),
+            nn.Conv2d(32, grid_features_dim, 3, padding="same", dtype=torch.double),
+            nn.BatchNorm2d(32, dtype=torch.double),
+            nn.SiLU(),
+        )
+
+        # Positional encoding
+        x_channel = torch.tensor([list(range(size))] * size, dtype=torch.double) / size
+        y_channel = x_channel.T
+        self.pos = torch.stack(
+            [x_channel, y_channel]
+        )  # Shape: (2, grid_size, grid_size)
+        self.pos.requires_grad = (
+            False  # TODO: Double check that the positional encoding doesn't change
+        )
+        self.use_pos = use_pos
+
+        # Fusion of object features into grid + scalar features
+        if objs_shape:
+            _, obj_dim = objs_shape
+            proj_dim = 64
+            n_heads = 4
+            self.proj = nn.Conv1d(obj_dim, proj_dim, 1)
+            self.attn1 = nn.MultiheadAttention(
+                proj_dim, n_heads, batch_first=True, dtype=torch.double
+            )
+            self.bn1 = nn.BatchNorm1d(grid_features_dim)
+            self.attn2 = nn.MultiheadAttention(
+                proj_dim, n_heads, batch_first=True, dtype=torch.double
+            )
+            self.bn2 = nn.BatchNorm1d(grid_features_dim)
+            self.attn3 = nn.MultiheadAttention(
+                proj_dim, n_heads, batch_first=True, dtype=torch.double
+            )
+            self.bn3 = nn.BatchNorm1d(grid_features_dim)
+
+        # Convert features into liklihood map
+        self.out_net = nn.Sequential(
+            nn.Conv2d(grid_features_dim, 32, 3, padding="same", dtype=torch.double),
             nn.BatchNorm2d(32, dtype=torch.double),
             nn.SiLU(),
             nn.Conv2d(32, 32, 3, padding="same", dtype=torch.double),
@@ -40,27 +90,56 @@ class MeasureModel(nn.Module):
             nn.Conv2d(32, 1, 3, padding="same", dtype=torch.double),
             nn.Sigmoid(),
         )
-        x_channel = torch.tensor([list(range(size))] * size, dtype=torch.double) / size
-        y_channel = x_channel.T
-        self.pos = torch.stack(
-            [x_channel, y_channel]
-        )  # Shape: (2, grid_size, grid_size)
-        self.pos.requires_grad = False
-        self.use_pos = use_pos
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(
+        self,
+        grid: Tensor,  # Shape: (batch_size, channels, size, size)
+        objs: Tensor,  # Shape: (batch_size, max_obj_size, obj_dim)
+        objs_attn_mask: Tensor,  # Shape: (batch_size, max_obj_size)
+    ) -> Tensor:
+        # Concat pos encoding to grid
+        device = grid.device
         if self.use_pos:
-            x = torch.concat(
+            grid = torch.concat(
                 [
-                    x,
+                    grid,
                     self.pos.unsqueeze(0)
-                    .tile([x.shape[0], 1, 1, 1])
-                    .to(device=x.device),
+                    .tile([grid.shape[0], 1, 1, 1])
+                    .to(device=device),
                 ],
                 dim=1,
             )
-        x = self.net(x).squeeze(1)  # Shape: (batch_size, grid_size, grid_size)
-        return x
+        grid_features = self.grid_net(
+            grid
+        )  # Shape: (batch_size, grid_features_dim, grid_size, grid_size)
+
+        if self.use_objs:
+            objs = self.proj(objs.permute(0, 2, 1)).permute(
+                0, 2, 1
+            )  # Shape: (batch_size, max_obj_size, proj_dim)
+            grid_features = grid_features.permute(
+                0, 2, 3, 1
+            )  # Shape: (batch_size, grid_size, grid_size, grid_features_dim)
+            orig_shape = grid_features.shape
+            grid_features = grid_features.flatten(
+                1, 2
+            )  # Shape: (batch_size, grid_size * grid_size, grid_features_dim)
+            attns = [self.attn1, self.attn2, self.attn3]
+            bns = [self.bn1, self.bn2, self.bn3]
+            for attn, bn in zip(attns, bns):
+                grid_features = attn(
+                    query=grid_features,
+                    key=objs,
+                    value=objs,
+                    key_padding_mask=objs_attn_mask,
+                )
+                grid_features = bn(grid_features.permute(0, 2, 1)).permute(0, 2, 1)
+                grid_features = nn.functional.silu(grid_features)
+            grid_features = grid_features.reshape(orig_shape).permute(
+                0, 3, 1, 2
+            )  # Shape: (batch_size, grid_features_dim, grid_size, grid_size)
+
+        return self.out_net(grid_features).squeeze(1) # Shape: (batch_size, grid_size, grid_size)
 
 
 def predict(belief: Tensor) -> Tensor:
