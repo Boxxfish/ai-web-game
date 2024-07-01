db37188b1ade2bb1bf63a5409de1bb1c96468483
Ben Giacalone <bensgiacalone@gmail.com>
2024-07-01 15:59:31 -0400

    Add model loading for filter model

--- webgame-game/src/filter.rs
+++ webgame-game/src/filter.rs
@@ -1,11 +1,24 @@
 use bevy::prelude::*;
 
+use crate::{
+    models::MeasureModel,
+    net::{load_weights_into_net, NNWrapper},
+};
+
 /// Plugin for Bayes filtering functionality.
 pub struct FilterPlugin;
 
 impl Plugin for FilterPlugin {
+    fn build(&self, app: &mut App) {}
+}
+
+/// Adds playable functionality to `FilterPlugin`.
+pub struct FilterPlayPlugin;
+
+impl Plugin for FilterPlayPlugin {
     fn build(&self, app: &mut App) {
-        ;
+        app.add_systems(Startup, init_filter_net)
+            .add_systems(Update, load_weights_into_net::<MeasureModel>);
     }
 }
 
@@ -13,4 +26,11 @@ impl Plugin for FilterPlugin {
 #[derive(Component)]
 pub struct BayesFilter {
     pub probs: Vec<f32>,
+}
+
+/// Initializes the filter network.
+fn init_filter_net(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(NNWrapper::<MeasureModel>::with_sftensors(
+        asset_server.load("model.safetensors"),
+    ));
 }
