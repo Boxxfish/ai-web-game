ba4e1aab016d99b954b27693470f29a440facb11
Ben Giacalone <bensgiacalone@gmail.com>
2024-07-17 14:05:23 -0400

    Make envs terminate when player is caught

--- webgame-ml/webgame/envs.py
+++ webgame-ml/webgame/envs.py
@@ -108,9 +108,10 @@ class GameEnv(pettingzoo.ParallelEnv):
             "player": -float(seen_player),
             "pursuer": float(seen_player),
         }
+        done = obs["pursuer"][0][4] > 0.5
         dones = {
-            "player": False,
-            "pursuer": False,
+            "player": done,
+            "pursuer": done,
         }
         truncs = {
             "player": trunc,
@@ -161,7 +162,7 @@ class GameEnv(pettingzoo.ParallelEnv):
 
     @functools.lru_cache(maxsize=None)
     def action_space(self, _agent: str) -> gym.Space:
-        return gym.spaces.Discrete(10)
+        return gym.spaces.Discrete(9)
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, _: str) -> gym.Space:
